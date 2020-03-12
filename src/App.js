177e08146d918f65cc5f3a7d3a685b56da1e9e5b
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-03-11 22:34:26 -0400

    state tasks rendering

--- src/App.js
+++ src/App.js
@@ -1,13 +1,92 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+
+
+const stockTasks = [
+  {
+    task: 'Shower',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Get Dressed',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Eat Breakfast',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Brush Teeth',
+    id: Date.now(),
+    completed: false
+  }
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      tasks: stockTasks
+    };
+  }
+
+toggleCompleted = todoId => {
+  console.log("app.js toggleCompleted", todoId);
+  this.setState({
+    tasks: this.state.tasks.map(todo => {
+      if (todo.id === todoId) {
+        return {
+          ...todo,
+          completed: !todo.completed
+        };
+      }
+      return todo;
+    })
+  });
+};
+
+addTask = taskName => {
+  this.setState({
+    tasks: [
+      ...this.state.tasks,
+      {
+        task: taskName,
+        id: Date.now(),
+        completed: false
+      }
+    ]
+  });
+};
+
+clearCompleted = () => {
+    console.log("app.js clear completed");
+    this.setState({
+      tasks: this.state.tasks.filter(todo => {
+        return !todo.completed;
+      })
+    });
+};
+
+
+
+
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className="Title">
+        <h2>Today's Task List</h2>
+      </div>
+      <div className="List">
+      <TodoList 
+        tasks={this.state.tasks}
+        toggleCompleted={this.toggleCompleted}
+        clearCompleted={this.clearCompleted}
+        />
+      </div>   
       </div>
     );
   }
